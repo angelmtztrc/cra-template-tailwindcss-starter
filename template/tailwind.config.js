eafe53e5098b00447080d35dbea1a6524aeb200d
Angel Martinez <angel.martineztrc@gmail.com>
2021-01-18 19:28:10 -0600

    fix(template): :sparkles:  better syntax in purge

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,6 +1,6 @@
 // add your custom configurations
 module.exports = {
-  purge: ['src/**/*.js', 'src/**/*.jsx', 'public/*.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
