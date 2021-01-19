0d533f49b1df4806cad9c5e1ade94a4e3bbeb520
Angel Martinez <angel.martineztrc@gmail.com>
2021-01-18 19:42:50 -0600

    feat(src/App.js): :sparkles:  better initial screen

--- template/src/App.js
+++ template/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="text-center">
-      <header className="flex flex-col items-center justify-center min-h-screen bg-secondary">
+      <header className="flex flex-col items-center justify-center min-h-screen font-sans bg-secondary">
         <img
           src={logo}
           style={{ height: '40vmin' }}
@@ -11,16 +11,30 @@ function App() {
           alt="logo"
         />
         <p className="text-white">
-          Edit <code>src/App.js</code> and save to reload.
+          Edit{' '}
+          <code className="font-monospace p-1 bg-gray-700 rounded">
+            src/App.js
+          </code>{' '}
+          and save to reload.
         </p>
-        <a
-          className="text-primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="flex items-center mt-2 space-x-8">
+          <a
+            className="text-primary cursor-pointer"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn ReactJS
+          </a>
+          <a
+            className="text-primary cursor-pointer"
+            href="https://tailwindcss.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Tailwind CSS
+          </a>
+        </div>
       </header>
     </div>
   );
